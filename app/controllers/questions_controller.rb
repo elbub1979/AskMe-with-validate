9ec3998755c948d9e207f4143e229abf1971e998
elbub1979 <ilya.leuh09@gmail.com>
2022-04-28 23:42:51 +0300

    added validates for user, question; output of errors corresponding to views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,36 +1,43 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: %i[update show destroy edit]
-  def create 
-    question = Question.create(question_params)
-
-    redirect_to question_path(question), notice: 'Новый вопрос создан!'
-  end
-
-  def update
-    @question.update(question_params)
-
-    redirect_to question_path(@question), notice: 'Сохранили вопрос!'
-  end
-
-  def destroy
-    @question.destroy
-
-    redirect_to questions_path, notice: 'Вопрос удалён!'
-  end
-
-  def show
-  end
 
   def index
     @question = Question.new
     @questions = Question.all
   end
 
+  def show; end
+
   def new
     @question = Question.new
   end
 
-  def edit
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to question_path(@question), notice: 'Новый вопрос создан!'
+    else
+      flash[:alert] = 'Вы неправильно заполнили поля формы регистрации'
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @question.update(question_params)
+      redirect_to question_path(@question), notice: 'Вопрос обновлён!'
+    else
+      flash.now[:alert] = 'При попытыке сохранить пользователя возникли ошибки'
+      render :edit
+    end
+  end
+
+  def destroy
+    @question.destroy
+
+    redirect_to questions_path, notice: 'Вопрос удалён!'
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
+  before_save :downcase_nickname
+
   has_secure_password
+
+  # nickname validates
+  validates :nickname, uniqueness: true, length: { maximum: 40 }, format: { with: /\A\w+\z/ }
+
+  # email validates
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  def downcase_nickname
+    nickname.downcase!
+  end
 end
