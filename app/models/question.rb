0795db797e0c397ad967bf59a07d005b91a191ab
elbub1979 <ilya.leuh09@gmail.com>
2022-05-04 21:57:44 +0300

    refactoring _question.html.erb, added simple_format to question.body

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,10 @@
 class Question < ApplicationRecord
-  before_save :remove_tags
+  # before_save :remove_tags
   validates :body, presence: true, length: { maximum: 280 }
 
   private
 
-  def remove_tags
-    body.gsub!(/(?:<).*?(?:>)/, '')
-  end
+  #  def remove_tags
+  #    body.gsub!(/(?:<).*?(?:>)/, '')
+  # end
 end
